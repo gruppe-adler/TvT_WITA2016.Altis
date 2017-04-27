6aaf0a326d3b32bdc14537a8f4a61cde77a8fb9e
XiviD <just_jus@hotmail.de>
2017-04-27 17:41:39 +0200

    3 Neue Caches + Stories

--- cfgCacheStories.hpp
+++ cfgCacheStories.hpp
@@ -12,4 +12,24 @@ class cfgCacheStories {
         onCoast = 0;
         script = "cacheStories\heliCrash1.sqf";
     };
+
+    class heliCrash2 {
+        size = 10;
+        onRoad = 0;
+        onCoast = 0;
+        script = "cacheStories\heliCrash2.sqf";
+    };
+
+    class breakingContact {
+        size = 10;
+        onRoad = 1;
+        onCoast = 0;
+        script = "cacheStories\breakingContact.sqf";
+    };
+
+
+
+
+
+
 };
--- cfgCaches.hpp
+++ cfgCaches.hpp
@@ -1,26 +1,62 @@
 class cfgCaches {
-    class exampleCache {
+
+//Cache 1
+    class Rifle {
         probability = 1;
+
+        //Rifles
 
         riflesWest = 1;
         riflesEast = 0;
-        magazinesPerRifle = 3;
+
+        //Snipers
+
+        sniperWest = 0;
+        sniperEast = 0;
+
+
+        //Handguns
 
         handguns = 1;
-        magazinesPerHandgun = 2;
 
-        launchers = 0;
+        //Launchers
+
+        launchersWest = 0;
+        launchersEast = 0;
+
+        //Ammo
+
         magazinesPerLauncher = 0;
+        magazinesPerHandgun = 2;
+        magazinesPerRifle = 4;
 
-        accessoriesWest = 2;
+        //Accessories
+
+        accessoriesWest = 3;
         accessoriesEast = 0;
+
+        //Sights
+
         sightsWest = 1;
         sightsEast = 0;
 
-        grenades = 3;
+        //Scopes
+
+        scopesWest = 0;
+        scopesEast = 0;
+
+        //SniperScopes
+
+        sniperScopesWest = 0;
+        sniperScopesEast = 0;
+
+        //Extras
+
+        grenades = 2;
         items = 2;
         medical = 5;
-        backpacks = 0;
+        backpacks = 1;
+
 
         cacheContainers[] = {
             "Box_NATO_WpsSpecial_F" //Welche Box sichtbar am Boden liegt
@@ -31,4 +67,151 @@ class cfgCaches {
             "heliCrash1"
         };
     };
+
+//Chache 2
+
+    class SniperCache {
+        probability = 0.2;
+
+        //Rifles
+
+        riflesWest = 0;
+        riflesEast = 0;
+
+        //Snipers
+
+        sniperWest = 1;
+        sniperEast = 0;
+
+
+        //Handguns
+
+        handguns = 1;
+
+        //Launchers
+
+        launchersWest = 0;
+        launchersEast = 0;
+
+        //Ammo
+
+        magazinesPerLauncher = 0;
+        magazinesPerHandgun = 2;
+        magazinesPerRifle = 3;
+
+        //Accessories
+
+        accessoriesWest = 2;
+        accessoriesEast = 0;
+
+        //Sights
+
+        sightsWest = 0;
+        sightsEast = 0;
+
+        //Scopes
+
+        scopesWest = 0;
+        scopesEast = 0;
+
+        //SniperScopes
+
+        sniperScopesWest = 1;
+        sniperScopesEast = 0;
+
+        //Extras
+
+        grenades = 2;
+        items = 2;
+        medical = 7;
+        backpacks = 1;
+
+
+        cacheContainers[] = {
+            "Box_NATO_WpsSpecial_F" //Welche Box sichtbar am Boden liegt
+        };
+
+        cacheStories[] = {
+            "hmmwvCrash1",
+            "breakingContact"
+        };
+    };
+
+//Cache 3
+
+    class LauncherCache {
+        probability = 0.2;
+
+        //Rifles
+
+        riflesWest = 0;
+        riflesEast = 0;
+
+        //Snipers
+
+        sniperWest = 0;
+        sniperEast = 0;
+
+
+        //Handguns
+
+        handguns = 1;
+
+        //Launchers
+
+        launchersWest = 1;
+        launchersEast = 0;
+
+        //Ammo
+
+        magazinesPerLauncher = 3;
+        magazinesPerHandgun = 2;
+        magazinesPerRifle = 0;
+
+        //Accessories
+
+        accessoriesWest = 2;
+        accessoriesEast = 0;
+
+        //Sights
+
+        sightsWest = 0;
+        sightsEast = 0;
+
+        //Scopes
+
+        scopesWest = 0;
+        scopesEast = 0;
+
+        //SniperScopes
+
+        sniperScopesWest = 1;
+        sniperScopesEast = 0;
+
+        //Extras
+
+        grenades = 2;
+        items = 2;
+        medical = 4;
+        backpacks = 1;
+
+
+        cacheContainers[] = {
+            "Box_NATO_WpsSpecial_F" //Welche Box sichtbar am Boden liegt
+        };
+
+        cacheStories[] = {
+            "hmmwvCrash1",
+            "breakingContact",
+            "heliCrash1",
+            "heliCrash2"
+        };
+    };
+
+
+
+
+
+
 };
+
